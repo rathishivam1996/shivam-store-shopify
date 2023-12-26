b1647829850366672fde8ac9e75ea29e91841a93
Shivam Rathi <shivamrathi2.sr@gmail.com>
2023-12-27 00:35:52 +0530

    test

--- assets/product.js
+++ assets/product.js
@@ -6,12 +6,10 @@ const productJs = {
   },
   setProduct(productJson) {
     this.product = productJson;
-    console.log(this.product, 'product');
   },
   handleVariantChange(defaultVariant) {
     // const productJsContext = this;
     const radioGroup = $(`#product-variant-radio-group-${this.handlizedId}`);
-
     // delegate event to radio group
     radioGroup.on('change', 'input:radio', () => {
       const selectedOptions = radioGroup
@@ -28,69 +26,71 @@ const productJs = {
       );
 
       // change url of the product page without refreshing page
-      const url = new URL(window.location.href);
-      url.searchParams.set('variant', matchedVariant.id);
-      if (window.history.replaceState) {
-        window.history.replaceState(null, this.product.title, url.toString());
-      } else {
-        // change location.href => reloads page!!!
-        window.location.href = url.toString();
-      }
+      this.updatePageUrlUtil(matchedVariant.id);
 
-      console.log(matchedVariant, 'matchedVariant');
       // change prices
-      $(`#product-price-${this.handlizedId} > strong`).text(
-        // eslint-disable-next-line no-undef
-        `${formatMoney(matchedVariant.price)}`,
-      );
-      const $compareAtPrice = $(
-        `#product-compare-at-price-${this.handlizedId} > strong`,
-      );
-      if (
-        matchedVariant.compare_at_price &&
-        matchedVariant.compare_at_price > matchedVariant.price
-      ) {
-        $compareAtPrice.parent().css('display', 'block');
-        // eslint-disable-next-line no-undef
-        $compareAtPrice.text(`${formatMoney(matchedVariant.compare_at_price)}`);
-      } else {
-        $compareAtPrice.text(``);
-        $compareAtPrice.parent().css('display', 'none');
-      }
+      this.changePriceUtil(matchedVariant.price, matchedVariant.compareAtPrice);
 
       // change featured image if selected variant has a featured image
-      let defaultAltText = `Group-${defaultVariant.options[0]}`;
       const currAltText = `Group-${matchedVariant.options[0]}`;
-      console.log(defaultAltText, "defaultAltText");
-      console.log(currAltText, "currAltText");
-      
-      if (matchedVariant.featured_image) {
-        $(`#product-featured-image-${this.handlizedId}`).attr(
-          'src',
-          matchedVariant.featured_image.src,
-        );
-        // remove thumb selector for prev variant
-        $(
-          `#product-thumb-container-${this.handlizedId}>img.selected`,
-        ).removeClass('selected');
-
-        $(`#product-thumb-container-${this.handlizedId}>img`).hide();
-        $(`#product-thumb-container-${this.handlizedId}>img[alt="${currAltText}"]`).show(); 
-
-        // add selected to new variant thumb
-        $(`#product-thumb-container-${this.handlizedId}>img`)
-          .eq(matchedVariant.featured_image.position - 1)
-          .addClass('selected');
-        // document
-        //   .querySelector('.product-thumb-container>img.selected')
-        //   .classList.remove('selected');
-
-        // document
-        //   .querySelectorAll('.product-thumb-container>img')
-        //   [matchedVariant.featured_image.position - 1].classList.add(
-        //     'selected',
-        //   );
-      }
+      this.updateImagesUtil(currAltText, matchedVariant.featured_image);
     });
+  },
+  updatePageUrlUtil(matchedVariantId) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('variant', matchedVariantId);
+    if (window.history.replaceState) {
+      window.history.replaceState(null, this.product.title, url.toString());
+    } else {
+      // change location.href => reloads page!!!
+      window.location.href = url.toString();
+    }
+  },
+  changePriceUtil(price, compareAtPrice) {
+    $(`#product-price-${this.handlizedId} > strong`).text(
+      // eslint-disable-next-line no-undef
+      `${formatMoney(price)}`,
+    );
+    const $compareAtPrice = $(
+      `#product-compare-at-price-${this.handlizedId} > strong`,
+    );
+    if (compareAtPrice && compareAtPrice > price) {
+      $compareAtPrice.parent().css('display', 'block');
+      // eslint-disable-next-line no-undef
+      $compareAtPrice.text(`${formatMoney(compareAtPrice)}`);
+    } else {
+      $compareAtPrice.text(``);
+      $compareAtPrice.parent().css('display', 'none');
+    }
+  },
+  updateImagesUtil(currAltText, featuredImage) {
+    // hide all images
+    $(`#product-thumb-container-${this.handlizedId}>img`).hide();
+    const $currAltImgs = $(
+      `#product-thumb-container-${this.handlizedId}>img[alt="${currAltText}"]`,
+    );
+    if ($currAltImgs.length) {
+      // show images with alt tag = curr selected variant
+      $currAltImgs.show();
+    } else {
+      // show all images as a fail safe
+      $(`#product-thumb-container-${this.handlizedId}>img`).show();
+    }
+
+    if (featuredImage) {
+      $(`#product-featured-image-${this.handlizedId}`).attr(
+        'src',
+        featuredImage.src,
+      );
+      // remove thumb selector for prev variant
+      $(
+        `#product-thumb-container-${this.handlizedId}>img.selected`,
+      ).removeClass('selected');
+
+      // add selected to new variant thumb
+      $(`#product-thumb-container-${this.handlizedId}>img`)
+        .eq(featuredImage.position - 1)
+        .addClass('selected');
+    }
   },
 };
